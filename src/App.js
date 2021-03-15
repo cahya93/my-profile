11510a02f564de28905c546274e8bbfa6fdf73b6
Eko Cahyanto <49233072+cahya93@users.noreply.github.com>
2021-03-15 23:46:08 +0700

    text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Ini aplikasi react pertama saya semoga bisa berkembang...</p>
+        <p>Ini aplikasi REACT pertama saya semoga bisa berkembang...</p>
         <p>Nama Saya Eko Cahyanto</p>
         <a className="App-link" target="_blank" rel="noopener noreferrer">
           Learn React
